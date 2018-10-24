03212515fc4425002bc1366ebb86c997ff9aab64
Andrew McCaffrey <andrew.mccaffrey@nist.gov>
2018-10-24 16:10:11 -0400

    Added back some changes that had accidentally been reverted

---
+++ src/main/java/gov/nist/fhir/adapter/forecaster/FullResults.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.nist.fhir.adapter.forecaster;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.tch.fc.model.ForecastActual;
+import org.tch.fc.model.ForecastEngineIssue;
+
+/**
+ *
+ * @author mccaffrey
+ */
+public class FullResults {
+
+    private List<ForecastActual> results = null;
+    private String tchLog = null;
+    private List<ForecastEngineIssue> issues = null;
+
+    /**
+     * @return the results
+     */
+    public List<ForecastActual> getResults() {
+        if (results == null) {
+            results = new ArrayList<>();
+        }
+        return results;
+    }
+
+    /**
+     * @param results the results to set
+     */
+    public void setResults(List<ForecastActual> results) {
+        this.results = results;
+    }
+
+    /**
+     * @return the tchLog
+     */
+    public String getTchLog() {
+        return tchLog;
+    }
+
+    /**
+     * @param tchLog the tchLog to set
+     */
+    public void setTchLog(String tchLog) {
+        this.tchLog = tchLog;
+    }
+
+    /**
+     * @return the issues
+     */
+    public List<ForecastEngineIssue> getIssues() {
+        if (issues == null) {
+            issues = new ArrayList<>();
+        }
+        return issues;
+    }
+
+    /**
+     * @param issues the issues to set
+     */
+    public void setIssues(List<ForecastEngineIssue> issues) {
+        this.issues = issues;
+    }
+}
